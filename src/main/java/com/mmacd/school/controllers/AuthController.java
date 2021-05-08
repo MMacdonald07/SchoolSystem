f4709f7a48eca48bd640eee05006dc86bccc8a05
Matthew <matthew.mmacdonald@gmail.com>
2021-05-08 13:14:12 +0100

    Completed log in endpoint

--- src/main/java/com/mmacd/school/controllers/AuthController.java
+++ src/main/java/com/mmacd/school/controllers/AuthController.java
@@ -2,13 +2,24 @@ package com.mmacd.school.controllers;
 
 import com.mmacd.school.payload.request.LoginRequest;
 import com.mmacd.school.payload.request.SignUpRequest;
+import com.mmacd.school.payload.response.JwtResponse;
+import com.mmacd.school.repository.RoleRepository;
+import com.mmacd.school.repository.UserRepository;
+import com.mmacd.school.security.jwt.JwtUtils;
+import com.mmacd.school.security.services.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -19,12 +30,38 @@ public class AuthController {
     private AuthenticationManager authenticationManager;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-        System.out.println("Logging in...");
-        return null;
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(), loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwt(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails
+                .getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(
+                jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles
+        ));
     }
 
     @PostMapping("/signup")
