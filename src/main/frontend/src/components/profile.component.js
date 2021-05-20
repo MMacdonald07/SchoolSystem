2a383910f783575740c9cec1ee860370842252dd
Matthew <matthew.mmacdonald@gmail.com>
2021-05-20 12:31:04 +0100

    Modified profile page to show subject and grade if available

--- src/main/frontend/src/components/profile.component.js
+++ src/main/frontend/src/components/profile.component.js
@@ -36,18 +36,17 @@ export default class Profile extends Component {
                             </h3>
                         </header>
                         <p>
-                            <strong>Token: </strong>{" "}
-                            {currentUser.accessToken.substring(0, 20)} ...{" "}
-                            {currentUser.accessToken.substr(
-                                currentUser.accessToken.length - 20
-                            )}
-                        </p>
-                        <p>
                             <strong>Id:</strong> {currentUser.id}
                         </p>
                         <p>
                             <strong>Email:</strong> {currentUser.email}
                         </p>
+                        {currentUser.subject && (<p>
+                            <strong>Subject:</strong> {currentUser.subject}
+                        </p>)}
+                        {currentUser.grade && (<p>
+                            <strong>Grade:</strong> {currentUser.grade}
+                        </p>)}
                         <strong>Authorities:</strong>
                         <ul>
                             {currentUser.roles &&
