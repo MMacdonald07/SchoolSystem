15a3dedd9b82c5381255fd94e6319cb83ab30e84
Matthew <matthew.mmacdonald@gmail.com>
2021-05-24 12:32:13 +0100

    Completed backend

--- src/main/java/com/mmacd/school/controllers/TestController.java
+++ src/main/java/com/mmacd/school/controllers/TestController.java
@@ -29,7 +29,7 @@ public class TestController {
 
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ADMIN')")
-    public String adminAccess() {
-        return "Admin";
+    public List<User> adminAccess() {
+        return userRepository.findAll();
     }
 }
