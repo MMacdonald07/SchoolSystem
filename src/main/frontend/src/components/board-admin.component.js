5d7b61b435e282c39a7970104f2e006a06070b58
Matthew <matthew.mmacdonald@gmail.com>
2021-05-25 12:49:41 +0100

    Modified admin frontend to display list of users and info on each of them

--- src/main/frontend/src/components/board-admin.component.js
+++ src/main/frontend/src/components/board-admin.component.js
@@ -7,7 +7,7 @@ export default class BoardAdmin extends Component {
         super(props);
 
         this.state = {
-            content: ""
+            content: []
         };
     }
 
@@ -35,8 +35,25 @@ export default class BoardAdmin extends Component {
         return (
             <div className="container">
                 <header className="jumbotron">
-                    <h3>{this.state.content}</h3>
+                    <h3>All users:</h3>
                 </header>
+                <ul>
+                    {this.state.content.map((user, index) => (
+                        <li key={index}>
+                            <p>User {user.id}:</p>
+                            <p>{user.username}</p>
+                            <p>{user.email}</p>
+                            {user.roles[0].name === "ROLE_STUDENT" ||
+                            user.roles[0].name === "ROLE_TEACHER" ? (
+                                <p>{user.subject}</p>
+                            ) : null}
+                            {user.roles[0].name === "ROLE_STUDENT" ? (
+                                <p>{user.grade}</p>
+                            ) : null}
+                            <p>{user.roles[0].name}</p>
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
